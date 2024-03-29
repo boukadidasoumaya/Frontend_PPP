9776af9f5045e881ef9d299477e6efdf70c409e2
mabahej <mabahej.benhassine@insat.ucar.tn>
2024-03-29 17:50:55 +0100

    fin charts

---
+++ src/Components/Charts/LinechartW.js
@@ -0,0 +1,130 @@
+import { ResponsiveLine } from '@nivo/line'
+import  {dataW} from './dataW'
+
+function LinechartW(data ) {
+  return (
+    <>
+     <ResponsiveLine
+        data={dataW}
+        theme={{
+          
+            "text": {
+                "fontSize": 11,
+                "fill": "#64738C",
+                "outlineWidth": 0,
+                "outlineColor": "transparent"
+            },
+            "axis": {
+                "domain": {
+                    "line": {
+                        "stroke": "#19204D",
+                        "strokeWidth": 0
+                    }
+                },
+                "legend": {
+                    "text": {
+                        "fontSize": 12,
+                        "fill": "#C9C8C8",
+                        "outlineWidth": 0,
+                        "outlineColor": "transparent"
+                    }
+                },
+                "ticks": {
+                    "line": {
+                        "stroke": "#64738C",
+                        "strokeWidth": 1
+                    },
+                    "text": {
+                        "fontSize": 11,
+                        "fill": "#64738C",
+                        "outlineWidth": 0,
+                        "outlineColor": "#64738C",
+                    }
+                }
+            },
+            "grid": {
+                "line": {
+                    "stroke": "#DDDBDB",
+                    "strokeWidth": 1
+                }
+            },
+            "legends": {
+                "title": {
+                    "text": {
+                        "fontSize": 11,
+                        "fill": "#DDDBDB",
+                        "outlineWidth": 0,
+                        "outlineColor": "transparent"
+                    }
+                },
+                "text": {
+                    "fontSize": 11,
+                    "fill": "##64738C",
+                    "outlineWidth": 0,
+                    "outlineColor": "transparent"
+                },
+                "ticks": {
+                    "line": {},
+                    "text": {
+                        "fontSize": 10,
+                        "fill": "##64738C",
+                        "outlineWidth": 0,
+                        "outlineColor": "transparent"
+                    }
+                }
+            },
+            
+            
+             
+          
+        }}
+          margin={{ top: 5, right: 110, bottom: 50, left: 60 }}
+          xScale={{ type: 'point' }}
+          yScale={{
+              type: 'linear',
+              min: 'auto',
+              max: 'auto',
+              stacked: true,
+              reverse: false
+          }}
+          yFormat=" >-.2f"
+          curve="natural"
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+              tickSize: 0,
+              tickPadding: 15,
+              tickRotation: 0,
+              legend: '',
+              legendOffset: 36,
+              legendPosition: 'middle',
+              truncateTickAt: 0
+          }}
+          axisLeft={{
+              tickSize: 0,
+              tickPadding: 15,
+              tickRotation: 0,
+              legend: '',
+              legendOffset: -40,
+              legendPosition: 'middle',
+              truncateTickAt: 0
+          }}
+          colors={['#5E72E4']}
+  
+          enableGridX={false}
+          enableGridY={false}
+          lineWidth={6}
+          pointSize={10}
+          pointColor={{ theme: 'background' }}
+          pointBorderWidth={2}
+          pointBorderColor={{ from: 'serieColor' }}
+          pointLabelYOffset={-12}
+          areaBaselineValue={20}
+          enableTouchCrosshair={true}
+          useMesh={true}
+          legends={[]}
+      />
+      </>
+    );
+  }
+export default LinechartW;
--- src/chartsview/Index.js
+++ src/chartsview/Index.js
@@ -1,14 +1,12 @@
+import Linechart from '../Components/Charts/Linechart';
 import * as React from 'react';
 //import { LineChart } from '@mui/x-charts/LineChart';
 import { useState } from "react";
 // node.js library that concatenates classes (strings)
 import classnames from "classnames";
-import { ResponsiveLine } from '@nivo/line'
-// javascipt plugin for creating charts
-import Chart from "chart.js";
-// react plugin used to create charts
-import { Line, Bar } from "react-chartjs-2";
-// reactstrap components
+import routes from "../routes";
+
+import Barchart from '../Components/Charts/Barchart.js';// reactstrap components
 import {
   Button,
   Card,
@@ -25,34 +23,27 @@ import {
 } from "reactstrap";
 
 // core components
-
+import Sidebar from '../Components/Sidebar/Sidebar.js';
 import Header from "../Components/Header/Header.js";
 import { colors } from '@mui/material';
+import LinechartW from '../Components/Charts/LinechartW.js';
 
 const Index = (props) => {
-  const [activeNav, setActiveNav] = useState(1);
-  const [chartExample1Data, setChartExample1Data] = useState("data1");
+  const [activeNav, setActiveNav] = useState("M");
+  const [chartLineData, setChartLineData] = useState("dataM");
 
  
   const toggleNavs = (e, index) => {
     e.preventDefault();
     setActiveNav(index);
-    setChartExample1Data("data" + index);
+    setChartLineData("data" + index);
   };
-  const data0 = {
-    labels: ["May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
-
-    datasets: [{
-      label: 'My First Dataset',
-      data: [65, 59, 80, 81, 56, 55, 40],
-      fill: false,
-      borderColor: 'white',
-      tension: 0.1
-    }]
-  };
+ 
   return (
     <>
+  
       <Header />
+      
       {/* Page content */}
       <Container className="mt--7" fluid>
         <Row>
@@ -64,17 +55,17 @@ const Index = (props) => {
                     <h6 className="text-uppercase text-light ls-1 mb-1">
                       Overview
                     </h6>
-                    <h2 className="text-white mb-0">Sales value</h2>
+                    <h2 className="text-white mb-0">taux d'abscence</h2>
                   </div>
                   <div className="col">
                     <Nav className="justify-content-end" pills>
                       <NavItem>
                         <NavLink
                           className={classnames("py-2 px-3", {
-                            active: activeNav === 1,
+                            active: activeNav === "M",
                           })}
                           href="#pablo"
-                          onClick={(e) => toggleNavs(e, 1)}
+                          onClick={(e) => toggleNavs(e,"M")}
                         >
                           <span className="d-none d-md-block">Month</span>
                           <span className="d-md-none">M</span>
@@ -83,11 +74,11 @@ const Index = (props) => {
                       <NavItem>
                         <NavLink
                           className={classnames("py-2 px-3", {
-                            active: activeNav === 2,
+                            active: activeNav === "W",
                           })}
                           data-toggle="tab"
                           href="#pablo"
-                          onClick={(e) => toggleNavs(e, 2)}
+                          onClick={(e) => toggleNavs(e, "W")}
                         >
                           <span className="d-none d-md-block">Week</span>
                           <span className="d-md-none">W</span>
@@ -99,188 +90,218 @@ const Index = (props) => {
               </CardHeader>
               <CardBody>
                 
-                <div className="chart">
-                <ResponsiveLine
-        data={[
-    
-          {
-            "id": "germany",
-            "color": "hsl(355, 70%, 50%)",
-            "data": [
-              {
-                "x": "plane",
-                "y": 238
-              },
-              {
-                "x": "helicopter",
-                "y": 215
-              },
-              {
-                "x": "boat",
-                "y": 213
-              },
-              {
-                "x": "train",
-                "y": 9
-              },
-              {
-                "x": "subway",
-                "y": 291
-              },
-              {
-                "x": "bus",
-                "y": 51
-              },
-              {
-                "x": "car",
-                "y": 176
-              },
-              {
-                "x": "moto",
-                "y": 29
-              },
-              {
-                "x": "bicycle",
-                "y": 80
-              },
-              {
-                "x": "horse",
-                "y": 283
-              },
-              {
-                "x": "skateboard",
-                "y": 6
-              },
-              {
-                "x": "others",
-                "y": 2
-              }
-            ]
-          },
-          
-            
-        ]}
-        theme={{
-          
-          "text": {
-              "fontSize": 11,
-              "fill": "#DDDBDB",
-              "outlineWidth": 0,
-              "outlineColor": "transparent"
-          },
-          "axis": {
-              "domain": {
-                  "line": {
-                      "stroke": "#DDDBDB",
-                      "strokeWidth": 1
-                  }
-              },
-              "legend": {
-                  "text": {
-                      "fontSize": 12,
-                      "fill": "#DDDBDB",
-                      "outlineWidth": 0,
-                      "outlineColor": "transparent"
-                  }
-              },
-              "ticks": {
-                  "line": {
-                      "stroke": "#DDDBDB",
-                      "strokeWidth": 1
-                  },
-                  "text": {
-                      "fontSize": 11,
-                      "fill": "#DDDBDB",
-                      "outlineWidth": 0,
-                      "outlineColor": "#DDDBDB",
-                  }
-              }
-          },
-          "grid": {
-              "line": {
-                  "stroke": "#DDDBDB",
-                  "strokeWidth": 1
-              }
-          },
-          "legends": {
-              "title": {
-                  "text": {
-                      "fontSize": 11,
-                      "fill": "#DDDBDB",
-                      "outlineWidth": 0,
-                      "outlineColor": "transparent"
-                  }
-              },
-              "text": {
-                  "fontSize": 11,
-                  "fill": "#DDDBDB",
-                  "outlineWidth": 0,
-                  "outlineColor": "transparent"
-              },
-              "ticks": {
-                  "line": {},
-                  "text": {
-                      "fontSize": 10,
-                      "fill": "#DDDBDB",
-                      "outlineWidth": 0,
-                      "outlineColor": "transparent"
-                  }
-              }
-          },
-          
-          
-           
-        
-      }}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: 'point' }}
-        yScale={{
-            type: 'linear',
-            min: 'auto',
-            max: 'auto',
-            stacked: true,
-            reverse: false
-        }}
-        yFormat=" >-.2f"
-        curve="natural"
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            tickSize: 0,
-            tickPadding: 15,
-            tickRotation: 0,
-            legend: '',
-            legendOffset: 36,
-            legendPosition: 'middle',
-            truncateTickAt: 0
-        }}
-        axisLeft={{
-            tickSize: 0,
-            tickPadding: 15,
-            tickRotation: 0,
-            legend: '',
-            legendOffset: -40,
-            legendPosition: 'middle',
-            truncateTickAt: 0
-        }}
-        colors={['#EDEDED']}
-
-        enableGridX={false}
-        enableGridY={false}
-        lineWidth={6}
-        pointSize={10}
-        pointColor={{ theme: 'background' }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: 'serieColor' }}
-        pointLabelYOffset={-12}
-        areaBaselineValue={20}
-        enableTouchCrosshair={true}
-        useMesh={true}
-        legends={[]}
-    />
-                </div>
+                <div className="chart">{
+                  chartLineData==="dataM"?<Linechart  /> :<LinechartW />}
+                            </div>
                 
                 </CardBody>
+            </Card>
+          </Col>
+          <Col xl="4">
+            <Card className="shadow">
+              <CardHeader className="bg-transparent">
+                <Row className="align-items-center">
+                  <div className="col">
+                    <h6 className="text-uppercase text-muted ls-1 mb-1">
+                      Performance
+                    </h6>
+                    <h2 className="mb-0">absence par filiaire </h2>
+                  </div>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                {/* Chart */}
+                <div className="chart">
+                  <Barchart />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
+        <Row className="mt-5">
+          <Col className="mb-5 mb-xl-0" xl="8">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <Row className="align-items-center">
+                  <div className="col">
+                    <h3 className="mb-0">nombre des etudiants et des professeurs</h3>
+                  </div>
+                  <div className="col text-right">
+                    <Button
+                      color="primary"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                      size="sm"
+                    >
+                      See all
+                    </Button>
+                  </div>
+                </Row>
+              </CardHeader>
+              <Table className="align-items-center table-flush" responsive>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">status</th>
+                    <th scope="col">nomber total</th>
+                    <th scope="col">nombre d'absence global</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">profs</th>
+                    <td>4,569</td>
+                    <td>340</td>
+                    <td>
+                      <i className="fas fa-arrow-up text-success mr-3" /> 46,53%
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">etudiants 1ere</th>
+                    <td>3,985</td>
+                    <td>319</td>
+                    <td>
+                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
+                      46,53%
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">etudiants 2eme</th>
+                    <td>3,513</td>
+                    <td>294</td>
+                    <td>
+                      <i className="fas fa-arrow-down text-warning mr-3" />{" "}
+                      36,49%
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">etudiant 3eme</th>
+                    <td>2,050</td>
+                    <td>147</td>
+                    <td>
+                      <i className="fas fa-arrow-up text-success mr-3" /> 50,87%
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">etudiant 5eme</th>
+                    <td>1,795</td>
+                    <td>190</td>
+                    <td>
+                      <i className="fas fa-arrow-down text-danger mr-3" />{" "}
+                      46,53%
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
+             
+            </Card>
+            
+          </Col>
+          <Col xl="4">
+            <Card className="shadow">
+              <CardHeader className="border-0">
+                <Row className="align-items-center">
+                  <div className="col">
+                    <h3 className="mb-0">Social traffic</h3>
+                  </div>
+                  <div className="col text-right">
+                    <Button
+                      color="primary"
+                      href="#pablo"
+                      onClick={(e) => e.preventDefault()}
+                      size="sm"
+                    >
+                      See all
+                    </Button>
+                  </div>
+                </Row>
+              </CardHeader>
+              <Table className="align-items-center table-flush" responsive>
+                <thead className="thead-light">
+                  <tr>
+                    <th scope="col">Referral</th>
+                    <th scope="col">Visitors</th>
+                    <th scope="col" />
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">Facebook</th>
+                    <td>1,480</td>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <span className="mr-2">60%</span>
+                        <div>
+                          <Progress
+                            max="100"
+                            value="60"
+                            barClassName="bg-gradient-danger"
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Facebook</th>
+                    <td>5,480</td>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <span className="mr-2">70%</span>
+                        <div>
+                          <Progress
+                            max="100"
+                            value="70"
+                            barClassName="bg-gradient-success"
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Google</th>
+                    <td>4,807</td>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <span className="mr-2">80%</span>
+                        <div>
+                          <Progress max="100" value="80" />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">Instagram</th>
+                    <td>3,678</td>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <span className="mr-2">75%</span>
+                        <div>
+                          <Progress
+                            max="100"
+                            value="75"
+                            barClassName="bg-gradient-info"
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">twitter</th>
+                    <td>2,645</td>
+                    <td>
+                      <div className="d-flex align-items-center">
+                        <span className="mr-2">30%</span>
+                        <div>
+                          <Progress
+                            max="100"
+                            value="30"
+                            barClassName="bg-gradient-warning"
+                          />
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </tbody>
+              </Table>
             </Card>
           </Col>
         </Row>
